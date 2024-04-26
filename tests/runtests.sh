37a87e9433fbb816219c708435c8838e69227f01
vladimir.babaev <vladimir.babaev.12@gmail.com>
2024-04-26 20:30:39 +0300

    chmod

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 export PYTHONPATH="./"
-export DJANGO_SETTINGS_MODULE='settings'
+export DJANGO_SETTINGS_MODULE='settings.py'
 
 if [ `which coverage` ] ; then
     export COVERAGE='coverage run'
