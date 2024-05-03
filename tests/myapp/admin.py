ebbcc47c672a6d15b432a4abb02577cd544b9137
vladimir.babaev <vladimir.babaev.12@gmail.com>
2024-05-03 18:42:26 +0300

    edits

--- tests/myapp/admin.py
+++ tests/myapp/admin.py
@@ -4,27 +4,11 @@ from django.contrib import admin
 
 from myapp.models import Category
 from django_admin_tree.admin import TreeParentAdminMixin
-from django_admin_tree.widgets import GfkLookupWidget
 
 
 @admin.register(Category)
 class CategoryAdmin(TreeParentAdminMixin, admin.ModelAdmin):
     list_display = ('name', 'parent')
     max_tree_depth = 3
+    raw_id_fields = ('parent',)
 
-
-    # def formfield_for_dbfield(self, db_field, **kwargs):
-    #     if db_field.name == 'name':
-    #         ct_model = namedtuple('ct_model', [
-    #             'model',
-    #             'test'
-    #         ])
-    #         ct = ct_model('test', 'test')
-    #         kwargs['widget'] = GfkLookupWidget(
-    #             content_type_field_name=ct,
-    #             parent_field=Category._meta.get_field('parent'),
-    #             app_label=Category._meta.app_label,
-    #             model_name=Category._meta.model_name
-    #         )
-    #
-    #     return super().formfield_for_dbfield(db_field, **kwargs)
